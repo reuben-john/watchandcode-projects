bf29f3b6834407d7ccb7099054ffc41c21efbeea
Reuben <rjkoppy@gmail.com>
2018-03-30 14:19:45 -0400

    Make displayTodos run when clicking button

--- script.js
+++ script.js
@@ -65,3 +65,10 @@ var todoList = {
     this.displayTodos();
   }
 };
+
+var displayTodosButton = document.getElementById("displayTodosButton");
+console.log(displayTodosButton);
+
+displayTodosButton.addEventListener("click", function() {
+  todoList.displayTodos();
+});
