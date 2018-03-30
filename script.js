6f52b52dae86ae130c54be2f7c96b2a3de1abc10
Reuben <rjkoppy@gmail.com>
2018-03-30 14:23:21 -0400

    Make toggle all button run toggleAll function

--- script.js
+++ script.js
@@ -67,8 +67,12 @@ var todoList = {
 };
 
 var displayTodosButton = document.getElementById("displayTodosButton");
-console.log(displayTodosButton);
+var toggleAllButton = document.getElementById("toggleAllButton");
 
 displayTodosButton.addEventListener("click", function() {
   todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener("click", function() {
+  todoList.toggleAll();
 });
