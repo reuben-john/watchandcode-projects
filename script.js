7978c087eb19b2958351c26034691e61badaa9b7
Reuben <rjkoppy@gmail.com>
2018-03-30 11:51:13 -0400

    Add comments to file

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Basic todo list
 var todoList = {
-  todos: [],
+  todos: [], // Stores todo list
+  // Logs display list to console
   displayTodos: function() {
     console.log("My todos", this.todos);
   },
+  // Adds todo item to todo list
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
@@ -11,13 +13,17 @@ var todoList = {
     });
     this.displayTodos();
   },
+  // Changes text of todo item on list
   changeTodo: function(position, value) {
     this.todos[position].todoText = value;
     this.displayTodos();
   },
+  // Deletes todo from list
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
+  },
+  // Toggles completed boolean on todo
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
