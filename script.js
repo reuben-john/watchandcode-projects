25534c8c6d3fdad53923d8bd6f19e68b1ca67bb1
Reuben <rjkoppy@gmail.com>
2018-03-30 12:18:31 -0400

    Change displayTodos to loop over array of todos

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var todoList = {
   todos: [], // Stores todo list
   // Logs display list to console
   displayTodos: function() {
-    console.log("My todos", this.todos);
+    for (var i = 0; i < this.todos.length; i++)
+      console.log("My todos", this.todos[i].todoText);
   },
   // Adds todo item to todo list
   addTodo: function(todoText) {
