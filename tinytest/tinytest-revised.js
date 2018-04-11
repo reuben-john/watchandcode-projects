70f86ac54cfb435138d3bc99f9c9cbdd63c4cb13
Reuben <rjkoppy@gmail.com>
2018-04-11 16:02:57 -0400

    Switch to eslint + prettier to help facilitate learning

--- tinytest/tinytest-revised.js
+++ tinytest/tinytest-revised.js
@@ -38,69 +38,69 @@
  * MIT License. See https://github.com/joewalnes/jstinytest/
  */
 
-var TintyTestHelper = {
-  renderStats: function (tests, failures) {
-    let numberOfTests = Object.keys(tests).length
-    let successes = numberOfTests - failures
-    let summaryMessage = ('Ran ' + numberOfTests + ' tests: ' + successes + ' successes, ' + failures + ' failures')
-    let summaryElement = document.createElement('h1')
-    summaryElement.textContent = summaryMessage
-    document.body.appendChild(summaryElement)
+const TintyTestHelper = {
+  renderStats(tests, failures) {
+    const numberOfTests = Object.keys(tests).length;
+    const successes = numberOfTests - failures;
+    const summaryMessage = `Ran ${numberOfTests} tests: ${successes} successes, ${failures} failures`;
+    const summaryElement = document.createElement("h1");
+    summaryElement.textContent = summaryMessage;
+    document.body.appendChild(summaryElement);
   }
-}
+};
 
 const TinyTest = {
+  run(tests) {
+    let failures = 0;
 
-  run: function (tests) {
-    let failures = 0
-
-    for (let testName in tests) {
-      let testAction = tests[testName]
+    for (const testName in tests) {
+      const testAction = tests[testName];
       try {
-        testAction()
-        console.log('%c%s', 'color:green; font-weight:bold', testName)
+        testAction();
+        console.log("%c%s", "color:green; font-weight:bold", testName);
       } catch (e) {
-        failures++
-        console.groupCollapsed('%c%s', 'color:red; font-weight:bold', testName)
-        console.error(e.stack)
-        console.groupEnd()
+        failures++;
+        console.groupCollapsed("%c%s", "color:red; font-weight:bold", testName);
+        console.error(e.stack);
+        console.groupEnd();
       }
     }
-    setTimeout(function () { // Give document a chance to complete
+    setTimeout(() => {
+      // Give document a chance to complete
       if (window.document && document.body) {
-        document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999')
-        TintyTestHelper.renderStats(tests, failures)
+        document.body.style.backgroundColor =
+          failures == 0 ? "#99ff99" : "#ff9999";
+        TintyTestHelper.renderStats(tests, failures);
       }
-    }, 0)
+    }, 0);
   },
 
-  fail: function (msg) {
-    throw new Error('fail(): ' + msg)
+  fail(msg) {
+    throw new Error(`fail(): ${msg}`);
   },
 
-  assert: function (value, msg) {
+  assert(value, msg) {
     if (!value) {
-      throw new Error('assert(): ' + msg)
+      throw new Error(`assert(): ${msg}`);
     }
   },
 
-  assertEquals: function (expected, actual) {
+  assertEquals(expected, actual) {
     if (expected != actual) {
-      throw new Error('assertEquals() "' + expected + '" != "' + actual + '"')
+      throw new Error(`assertEquals() "${expected}" != "${actual}"`);
     }
   },
 
-  assertStrictEquals: function (expected, actual) {
+  assertStrictEquals(expected, actual) {
     if (expected !== actual) {
-      throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"')
+      throw new Error(`assertStrictEquals() "${expected}" !== "${actual}"`);
     }
   }
+};
 
-}
-
-const fail = TinyTest.fail
-const assert = TinyTest.assert
-const assertEquals = TinyTest.assertEquals
-const eq = TinyTest.assertEquals // alias for assertEquals
-const assertStrictEquals = TinyTest.assertStrictEquals
-const tests = TinyTest.run
+const fail = TinyTest.fail;
+const assert = TinyTest.assert;
+const assertEquals = TinyTest.assertEquals;
+const eq = TinyTest.assertEquals; // alias for assertEquals
+const assertStrictEquals = TinyTest.assertStrictEquals;
+const tests = TinyTest.run;
