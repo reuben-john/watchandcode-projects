0bd0174e7b8547138108edaf499ded467f6e0386
Reuben <rjkoppy@gmail.com>
2018-04-25 12:33:52 -0400

    Create third test

--- beasts/libraryStestem-Revised/tests/spec/LibrarySpec.js
+++ beasts/libraryStestem-Revised/tests/spec/LibrarySpec.js
@@ -1,27 +1,3 @@
-librarySystem("dependency", [], function() {
-  return "loaded dependency";
-});
-
-librarySystem("app", ["dependency"], function(dependency) {
-  return "app with " + dependency;
-});
-
-librarySystem("app"); // 'app with loaded dependency'
-
-librarySystem("name", [], function() {
-  return "Gordon";
-});
-
-librarySystem("company", [], function() {
-  return "Watch and Code";
-});
-
-librarySystem("workBlurb", ["name", "company"], function(name, company) {
-  return name + " works at " + company;
-});
-
-librarySystem("workBlurb"); // 'Gordon works at Watch and Code'
-
 describe("librarySystem Tests", function() {
   it("should be able to create a library with zero dependencies", function() {
     librarySystem("dependency", [], function() {
@@ -34,5 +10,19 @@ describe("librarySystem Tests", function() {
       return "app with " + dependency;
     });
     expect(librarySystem("app")).toBe("app with loaded dependency");
+  });
+  it("should be able to create a library with two or more dependencies", function() {
+    librarySystem("name", [], function() {
+      return "Gordon";
+    });
+
+    librarySystem("company", [], function() {
+      return "Watch and Code";
+    });
+
+    librarySystem("workBlurb", ["name", "company"], function(name, company) {
+      return name + " works at " + company;
+    });
+    expect(librarySystem("workBlurb")).toBe("Gordon works at Watch and Code");
   });
 });
