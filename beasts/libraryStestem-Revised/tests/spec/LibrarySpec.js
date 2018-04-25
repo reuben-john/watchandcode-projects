0a01fce3c91d1b0fe46f7ce1b77b81600274cbce
Reuben <rjkoppy@gmail.com>
2018-04-25 16:18:27 -0400

    Add more test cases

--- beasts/libraryStestem-Revised/tests/spec/LibrarySpec.js
+++ beasts/libraryStestem-Revised/tests/spec/LibrarySpec.js
@@ -1,27 +1,57 @@
 describe("librarySystem Tests", function() {
   it("should be able to create a library with zero dependencies", function() {
+    librarySystem("newDependency", [], function() {
+      return "loaded dependency";
+    });
+    expect(librarySystem("newDependency")).toBe("loaded dependency");
+  });
+
+  it("should be able to create a library with one dependency", function() {
     librarySystem("dependency", [], function() {
       return "loaded dependency";
     });
-    expect(librarySystem("dependency")).toBe("loaded dependency");
-  });
-  it("should be able to create a library with one dependency", function() {
     librarySystem("app", ["dependency"], function(dependency) {
       return "app with " + dependency;
     });
     expect(librarySystem("app")).toBe("app with loaded dependency");
   });
+
   it("should be able to create a library with two or more dependencies", function() {
+    librarySystem("name", [], function() {
+      return "Gordon";
+    });
+    librarySystem("company", [], function() {
+      return "Watch and Code";
+    });
     librarySystem("workBlurb", ["name", "company"], function(name, company) {
       return name + " works at " + company;
     });
     expect(librarySystem("workBlurb")).toBe("Gordon works at Watch and Code");
   });
-  librarySystem("name", [], function() {
-    return "Gordon";
+
+  it("should load a library even when loaded out of order", function() {
+    librarySystem("cheeseBurger", ["cheese", "burger"], function(
+      cheese,
+      burger
+    ) {
+      return "Here, have a " + cheese + burger;
+    });
+    librarySystem("cheese", [], function() {
+      return "cheese";
+    });
+    librarySystem("burger", [], function() {
+      return "burger";
+    });
+    expect(librarySystem("cheeseBurger")).toBe("Here, have a cheeseburger");
   });
 
-  librarySystem("company", [], function() {
-    return "Watch and Code";
+  it("should only run the callback function once", function() {
+    var timesRun = 0;
+    librarySystem("counter", [], function(cheese, burger) {
+      timesRun++;
+    });
+    librarySystem("counter");
+    librarySystem("counter");
+    expect(timesRun).toBe(1);
   });
 });
