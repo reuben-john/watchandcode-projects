65413597a192204e7b38b67882614d0d6d81c496
Reuben <rjkoppy@gmail.com>
2018-04-25 10:54:07 -0400

    Create first test with jasmine and basic librarysystem function

--- beasts/librarysystem-revised.js
+++ beasts/librarysystem-revised.js
@@ -1,75 +1,50 @@
 // This is an example of a library that keeps the global namespace clean
 // by storing modules to a librarySystem
 
-// window.sandwichJS has an original value
-window.sandwichLibrary = "Library with books and sandwiches";
-
-//SandwichJS loads'
-function librarySystem(libraryName, dependencies, callback) {
-  if (arguments.length > 1) {
-    libraryStorage[libraryName] = callback();
-  } else {
-    return libraryStorage[libraryName];
-  }
-}
-
 (function() {
-  var breads = {
-    wheat: "A healthy option",
-    white: "An unhealthy option"
-  };
+  var libraryStorage = {};
 
-  var fillings = {
-    turkey: "For boring sandiwches",
-    cheese: "For the vegetarians"
-  };
-
-  var sandwichLibrary = {
-    breads: breads,
-    fillings: fillings
-  };
-
-  if (typeof librarySystem !== "undefined") {
-    // Handle librarySystem case
-    librarySystem("sandwichLibrary", function() {
-      return sandwichLibrary;
-    });
-  } else {
-    // Handle window case
-    var oldSandwichLibrary = window.sandwichLibrary;
-
-    sandwichLibrary.noConflict = function() {};
-    window.sandwichLibrary = oldSandwichLibrary;
-    return sandwichLibrary;
-
-    window.sandwichLibrary = sandwichLibrary;
+  function librarySystem(libraryName, dependencies, callback) {
+    if (arguments.length > 1) {
+      libraryStorage[libraryName] = callback();
+    } else {
+      return libraryStorage[libraryName];
+    }
   }
+  global.librarySystem = librarySystem;
 })();
-
-var sandwichJS = sandwichLibrary.noConflict();
 
 // Example functions
 
-librarySystem("dependency", [], function() {
-  return "loaded dependency";
+// librarySystem("dependency", [], function() {
+//   return "loaded dependency";
+// });
+
+// librarySystem("app", ["dependency"], function(dependency) {
+//   return "app with " + dependency;
+// });
+
+// librarySystem("app"); // 'app with loaded dependency'
+
+// librarySystem("name", [], function() {
+//   return "Gordon";
+// });
+
+// librarySystem("company", [], function() {
+//   return "Watch and Code";
+// });
+
+// librarySystem("workBlurb", ["name", "company"], function(name, company) {
+//   return name + " works at " + company;
+// });
+
+// librarySystem("workBlurb"); // 'Gordon works at Watch and Code'
+
+describe("librarySystem Tests", function() {
+  it("should be able to create a library with zero dependencies", function() {
+    librarySystem("dependency", [], function() {
+      return "test";
+    });
+    expect(librarySystem("dependency")).toBe("test");
+  });
 });
-
-librarySystem("app", ["dependency"], function(dependency) {
-  return "app with " + dependency;
-});
-
-librarySystem("app"); // 'app with loaded dependency'
-
-librarySystem("name", [], function() {
-  return "Gordon";
-});
-
-librarySystem("company", [], function() {
-  return "Watch and Code";
-});
-
-librarySystem("workBlurb", ["name", "company"], function(name, company) {
-  return name + " works at " + company;
-});
-
-librarySystem("workBlurb"); // 'Gordon works at Watch and Code'
