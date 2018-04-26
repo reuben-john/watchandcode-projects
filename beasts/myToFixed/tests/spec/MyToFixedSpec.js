d293a60eb79952969e6d56fd5130c0fd89da374c
Reuben <rjkoppy@gmail.com>
2018-04-26 09:20:28 -0400

    Add more test cases

--- beasts/myToFixed/tests/spec/MyToFixedSpec.js
+++ beasts/myToFixed/tests/spec/MyToFixedSpec.js
@@ -3,10 +3,21 @@ describe("My isPrototypeOf Tests", function() {
     expect(toFixedString([], 0).toString()).toBe(
       "TypeError: Please use a float or integer for the first argument"
     );
+    expect(toFixedString("10", 0).toString()).toBe(
+      "TypeError: Please use a float or integer for the first argument"
+    );
   });
+
   it("should return a TypeError when the precision is not a positive integer", function() {
     expect(toFixedString(10, -1).toString()).toBe(
       "TypeError: Please use a positive integer for the second argument"
     );
+    expect(toFixedString(10, "1").toString()).toBe(
+      "TypeError: Please use a positive integer for the second argument"
+    );
+  });
+
+  it("should shift the decimal of an int over by the precision amount", function() {
+    expect(toFixedString(10, 1)).toBe(10.0);
   });
 });
