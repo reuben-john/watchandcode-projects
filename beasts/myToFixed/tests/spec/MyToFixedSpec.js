4066a4fd056f1dc094763c351c7d3ddf8306938c
Reuben <rjkoppy@gmail.com>
2018-04-26 10:50:05 -0400

    Add tests for negative numbers

--- beasts/myToFixed/tests/spec/MyToFixedSpec.js
+++ beasts/myToFixed/tests/spec/MyToFixedSpec.js
@@ -21,13 +21,18 @@ describe("My isPrototypeOf Tests", function() {
     expect(toFixedString(10, 4)).toBe("10.0000");
   });
 
-  it("it should correctly round based on the precision value", function() {
+  it("should correctly round based on the precision value", function() {
     expect(toFixedString(0.615, 2)).toBe("0.62");
     expect(toFixedString(10.235, 2)).toBe("10.24");
     expect(toFixedString(1.005, 2)).toBe("1.01");
   });
 
-  it("it should append extra zeros to floats based on precision", function() {
+  it("should append extra zeros to floats based on precision", function() {
     expect(toFixedString(0.615, 4)).toBe("0.6150");
+  });
+
+  it("it should work with negative numbers", function() {
+    expect(toFixedString(-0.615, 4)).toBe("-0.6150");
+    expect(toFixedString(-22, 2)).toBe("-22.00");
   });
 });
